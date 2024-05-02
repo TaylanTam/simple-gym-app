3824fd27fcadbb6ece757257bc7298cc0b135d9a
Taylan Tam <tamoiyan731@gmail.com>
2024-05-02 17:02:20 +0800

    Revert "clone gym-log"
    This reverts commit 6d2b624d526f0a4cb8bf98d4c45d66cf44786db5.

--- frontend/src/api/profileApi.js
+++
@@ -1,5 +0,0 @@
-import axiosInstance from "./instances/axiosInstance";
-import {apiRoutes} from "../utils/routes";
-
-export const getProfile = () => axiosInstance.get(apiRoutes.profile).then(response => response.data[0]);
-export const updateProfile = (data) => axiosInstance.post(apiRoutes.profile, data).then(response => response.data);
--- frontend/src/utils/helpers.js
+++
@@ -1,4 +0,0 @@
-export const timestampToString = (timestamp) => {
-    const date = new Date(timestamp);
-    return date.toDateString().toLocaleString();
-}
